ac38cb0160ee363500ade40f060121d4ccd203ea
Your Name <you@example.com>
2022-08-07 21:16:42 +0300

    update 7/8

--- app.js
+++ app.js
@@ -1,277 +1,177 @@
-// variabels
-var srick = document.querySelector(".srick"),
-  all_i_ele = document.querySelectorAll("span"),
-  xmarkIcon = '<i class="fa-solid fa-xmark"></i>',
-  o_markIcon = '<i class="fa-solid fa-o"></i>',
-  changeIcons = xmarkIcon,
-  numberArray = new Array(all_i_ele.length),
-  play_button = document.getElementById("play"),
-  audio = new Audio("mouse_click.mp3"),
-  winnerAudio = new Audio("winner.wav"),
-  array_x_o = ["x", "o"],
-  personButton = document.querySelector("#person-button"),
-  startGame = document.querySelector(".play-with-div"),
-  playGame = document.querySelector("#playGame"),
-  pcButton = document.querySelector("#pc-button"),
-  winner;
-var cont;
-// Fill the array with a null value
-numberArray.fill(null);
-// functions
-personButton.addEventListener("click", personFun);
-pcButton.addEventListener("click",pcFun);
-play_button.addEventListener("click", playFun);
+class Main {
+  constructor() {
+    // variables
+    this.streak = document.querySelector(".streak");
+    this.all_i_ele = document.querySelectorAll("span");
+    this.x_icon = '<i class="fa-solid fa-xmark"></i>';
+    this.o_icon = '<i class="fa-solid fa-o"></i>';
+    // for change between x and o
+    this.changeIcons = this.x_icon;
+    // empty array to store the values x and o inside
+    this.numberArray = new Array(this.all_i_ele.length);
+    // Fill the array with a null value
+    this.numberArray.fill(null);
+    this.array_x_o = ["x", "o"];
+    // Button to choose a second person to play
+    this.personButton = document.querySelector("#person-button");
+    // Play with computer button
+    this.pcButton = document.querySelector("#pc-button");
+    // this is container
+    this.containerStartGame = document.querySelector(".play-with-div");
+    // Game restart container
+    this.containerPlayGame = document.querySelector("#containerPlayGame");
+    // Game restart Button
+    this.play_button = document.getElementById("play");
+    this.data = [
+      // row
+      { num: [0, 1, 2], streakClass: "streak-row-top" },
+      { num: [3, 4, 5], streakClass: "streak-row-center" },
+      { num: [6, 7, 8], streakClass: "streak-row-bottom" },
+      // column
+      { num: [0, 3, 6], streakClass: "streak-column-left" },
+      { num: [1, 4, 7], streakClass: "streak-column-center" },
+      { num: [2, 5, 8], streakClass: "streak-column-right" },
+      // diagonal
+      { num: [0, 4, 8], streakClass: "streak-diagonal-A" },
+      { num: [2, 4, 6], streakClass: "streak-diagonal-B" },
+    ];
+    this.winner;
+    this.cont;
+    // audio files
+    this.audio = new Audio("mouse_click.mp3");
+    this.winnerAudio = new Audio("winner.wav");
+    // functions
+    // Choose to start playing with a person or a pc
+    this.personButton.addEventListener("click", this.handlePersonFun);
 
-function personFun() {
-  all_i_ele.forEach((ele) => ele.addEventListener("click", main));
-  playGame.className="";
-  startGame.className="d-none"
+    // restart the game
+    this.play_button.addEventListener("click", this.playFun);
   }
-  
-  // تحتاج تكميل 
-function pcFun(){
-  playGame.className="";
-  startGame.className="d-none";
-  let number=rando(0, 8);
-  all_i_ele.forEach((ele) => ele.addEventListener("click", (event)=>{
-    let eventElements = event.target,
-    getClassElements = eventElements.className,
-    selectElement = document.querySelector(`.${getClassElements}`),
-    index_Element = eventElements.dataset.index;
-    if(changeIcons===xmarkIcon){
-      selectElement.innerHTML=xmarkIcon;
-      numberArray[index_Element-1]="x";
-      changeIcons = o_markIcon;
-      audio.play();
-      if(changeIcons == o_markIcon){
-        
-        if(numberArray[number]==null){
-          numberArray[number]="o";
-          all_i_ele[number].innerHTML=o_markIcon;
-          changeIcons = xmarkIcon;
-          audio.play();
-        }else{
-          var nullValue=numberArray.indexOf(null);
-          if(nullValue!== -1){
-            numberArray[nullValue]="o";
-            all_i_ele[nullValue].innerHTML=o_markIcon;
-            changeIcons = xmarkIcon;
-            audio.play();
-          }
-          
-        }
+  //this function Choose to start playing with a person or a pc
+  handlePersonFun = () => {
+    // Store and check values and display the result
+    this.all_i_ele.forEach((ele) =>
+      ele.addEventListener("click", this.handleChange)
+    );
+    // hide #containerStartGame
+    this.containerStartGame.className = "d-none";
+    // show #containerPlayGame
+    this.containerPlayGame.className = "";
+  };
+
+  CheckWinner = () => {
+    for (const iterator of this.data) {
+      var { num, streakClass } = iterator;
+      var tile1 = this.numberArray[num[0]];
+      var tile2 = this.numberArray[num[1]];
+      var tile3 = this.numberArray[num[2]];
+      if (tile1 != null && tile1 === tile2 && tile1 === tile3) {
+        this.all_i_ele.forEach((allElements) => (allElements.style.pointerEvents = "none"));
+        this.winner = tile1;
+        this.streak.classList.add(streakClass);
+        this.winnerAudio.play();
       }
-      
-    } else{
-      numberArray[number]="o";
-          all_i_ele[number].innerHTML=o_markIcon;
-          changeIcons = xmarkIcon;
-          audio.play();
     }
-    for (let index = 0; index < array_x_o.length; index++) {
-      let ele = array_x_o[index];
-  
-      switch (event.type == "click") {
-        // row
-        case numberArray[0] === ele &&
-          numberArray[1] === ele &&
-          numberArray[2] === ele:
-          winner = ele;
-          srick.classList.add("srick-row-top");
-          break;
-        case numberArray[3] === ele &&
-          numberArray[4] === ele &&
-          numberArray[5] === ele:
-          winner = ele;
-          srick.classList.add("srick-row-center");
-          break;
-        case numberArray[6] === ele &&
-          numberArray[7] === ele &&
-          numberArray[8] === ele:
-          winner = ele;
-          srick.classList.add("srick-row-bottom");
-          break;
-        // colomn
-        case numberArray[0] === ele &&
-          numberArray[3] === ele &&
-          numberArray[6] === ele:
-          winner = ele;
-          srick.classList.add("srick-colomn-left");
-          break;
-        case numberArray[1] === ele &&
-          numberArray[4] === ele &&
-          numberArray[7] === ele:
-          winner = ele;
-          srick.classList.add("srick-colomn-center");
-          break;
-        case numberArray[2] === ele &&
-          numberArray[5] === ele &&
-          numberArray[8] === ele:
-          winner = ele;
-          srick.classList.add("srick-colomn-right");
-          break;
-        case numberArray[1] === ele &&
-          numberArray[4] === ele &&
-          numberArray[7] === ele:
-          winner = ele;
-          srick.classList.add("srick-colomn-center");
-          break;
-        case numberArray[2] === ele &&
-          numberArray[5] === ele &&
-          numberArray[8] == ele:
-          winner = ele;
-          srick.classList.add("srick-colomn-right");
-          break;
-        // diagonal
-        case numberArray[0] === ele &&
-          numberArray[4] === ele &&
-          numberArray[8] == ele:
-          winner = ele;
-          srick.classList.add("srick-diagonal-A");
-          break;
-        case numberArray[2] === ele &&
-          numberArray[4] === ele &&
-          numberArray[6] == ele:
-          winner = ele;
-          srick.classList.add("srick-diagonal-B");
-          break;
-  
-        default:
-          break;
-      }
-  
-      if (srick.classList.length > 1) {
-        all_i_ele.forEach(
-          (allElements) => (allElements.style.pointerEvents = "none")
-        );
-        document.querySelector(".h2").innerText = ": الرابح هو";
-        document.querySelector(".span").innerText = winner;
-        winnerAudio.play();
-      } else {
+  };
+
+  // change between x and o
+  handleChange = (event) => {
+    var eventElements = event.target;
+    // Define each item via Class name
+    let selectElement = document.querySelector(`.${eventElements.className}`);
+    // Get the number of each item ( data-index="1" )
+    let index_Element = selectElement.dataset.index;
+
+    if (this.changeIcons === this.x_icon) {
+      selectElement.innerHTML = this.x_icon;
+      this.numberArray[index_Element - 1] = "x";
+      this.changeIcons = this.o_icon;
+      this.audio.play();
+    } else {
+      selectElement.innerHTML = this.o_icon;
+      this.numberArray[index_Element - 1] = "o";
+      this.changeIcons = this.x_icon;
+      this.audio.play();
+    }
+    // Check winner
+    this.CheckWinner();
+
+    if (event.target.innerHTML == this.x_icon) {
+      document.querySelector(".h2").innerText = " : الدور عند";
+      document.querySelector(".span").innerText = "o";
+    } else {
+      document.querySelector(".h2").innerText = " : الدور عند";
+      document.querySelector(".span").innerText = "x";
+    }
+  };
+  // restart the game function
+  playFun = () => {
+    this.all_i_ele.forEach((ele) => {
+      ele.innerHTML = "";
+      ele.style.pointerEvents = "auto";
+    });
+    this.streak.className = "streak";
+    this.numberArray.fill(null);
+  };
+}
+var person = new Main();
+class App extends Main {
+  constructor() {
+    // variables
+    super();
+    this.number = rando(0, 8);
+    this.pcButton.addEventListener("click", this.handleChangePc);
+  }
+  handleChangePc = () => {
+    // hide #containerStartGame
+    this.containerStartGame.className = "d-none";
+    // show #containerPlayGame
+    this.containerPlayGame.className = "";
+
+    this.all_i_ele.forEach((ele) =>
+      ele.addEventListener("click", (event) => {
+        let eventElements = event.target,
+          getClassElements = eventElements.className,
+          selectElement = document.querySelector(`.${getClassElements}`),
+          index_Element = eventElements.dataset.index;
+        if (this.changeIcons === this.x_icon) {
+          selectElement.innerHTML = this.x_icon;
+          this.numberArray[index_Element - 1] = "x";
+          this.changeIcons = this.o_icon;
+          this.audio.play();
+          if (this.changeIcons == this.o_icon) {
+            if (this.numberArray[this.number] == null) {
+              this.numberArray[this.number] = "o";
+              this.all_i_ele[this.number].innerHTML = this.o_icon;
+              this.changeIcons = this.x_icon;
+              this.audio.play();
+            } else {
+              var nullValue = this.numberArray.indexOf(null);
+              if (nullValue !== -1) {
+                this.numberArray[nullValue] = "o";
+                this.all_i_ele[nullValue].innerHTML = this.o_icon;
+                this.changeIcons = this.x_icon;
+                this.audio.play();
+              }
+            }
+          }
+        } else {
+          this.numberArray[this.number] = "o";
+          this.all_i_ele[this.number].innerHTML = this.o_icon;
+          this.changeIcons = this.x_icon;
+          this.audio.play();
+        }
+          // Check winner
+          this.CheckWinner();
+        if (this.streak.classList.length > 1) {
+          document.querySelector(".h2").innerText = ": الرابح هو";
+          document.querySelector(".span").innerText = this.winner;
+        } else {
           document.querySelector(".h2").innerText = " : الدور عند";
           document.querySelector(".span").innerText = "x";
-        
-      }
-    }
-}));// ele.addEventListener
-
+        }
+      })
+    );
+  };
 }
-
-function main(event) {
-  var eventElements = event.target,
-    getClassElements = eventElements.className,
-    selectElement = document.querySelector(`.${getClassElements}`),
-    index_Element = eventElements.dataset.index;
-  // change between x and o
-  if (changeIcons === xmarkIcon) {
-    selectElement.innerHTML = xmarkIcon;
-    numberArray[index_Element - 1] = "x";
-    changeIcons = o_markIcon;
-    audio.play();
-  } else {
-    selectElement.innerHTML = o_markIcon;
-    numberArray[index_Element - 1] = "o";
-    changeIcons = xmarkIcon;
-    audio.play();
-  }
-
-  for (let index = 0; index < array_x_o.length; index++) {
-    let ele = array_x_o[index];
-
-    switch (event.type == "click") {
-      // row
-      case numberArray[0] === ele &&
-        numberArray[1] === ele &&
-        numberArray[2] === ele:
-        winner = ele;
-        srick.classList.add("srick-row-top");
-        break;
-      case numberArray[3] === ele &&
-        numberArray[4] === ele &&
-        numberArray[5] === ele:
-        winner = ele;
-        srick.classList.add("srick-row-center");
-        break;
-      case numberArray[6] === ele &&
-        numberArray[7] === ele &&
-        numberArray[8] === ele:
-        winner = ele;
-        srick.classList.add("srick-row-bottom");
-        break;
-      // colomn
-      case numberArray[0] === ele &&
-        numberArray[3] === ele &&
-        numberArray[6] === ele:
-        winner = ele;
-        srick.classList.add("srick-colomn-left");
-        break;
-      case numberArray[1] === ele &&
-        numberArray[4] === ele &&
-        numberArray[7] === ele:
-        winner = ele;
-        srick.classList.add("srick-colomn-center");
-        break;
-      case numberArray[2] === ele &&
-        numberArray[5] === ele &&
-        numberArray[8] === ele:
-        winner = ele;
-        srick.classList.add("srick-colomn-right");
-        break;
-      case numberArray[1] === ele &&
-        numberArray[4] === ele &&
-        numberArray[7] === ele:
-        winner = ele;
-        srick.classList.add("srick-colomn-center");
-        break;
-      case numberArray[2] === ele &&
-        numberArray[5] === ele &&
-        numberArray[8] == ele:
-        winner = ele;
-        srick.classList.add("srick-colomn-right");
-        break;
-      // diagonal
-      case numberArray[0] === ele &&
-        numberArray[4] === ele &&
-        numberArray[8] == ele:
-        winner = ele;
-        srick.classList.add("srick-diagonal-A");
-        break;
-      case numberArray[2] === ele &&
-        numberArray[4] === ele &&
-        numberArray[6] == ele:
-        winner = ele;
-        srick.classList.add("srick-diagonal-B");
-        break;
-
-      default:
-        break;
-    }
-
-    if (srick.classList.length > 1) {
-      all_i_ele.forEach(
-        (allElements) => (allElements.style.pointerEvents = "none")
-      );
-      document.querySelector(".h2").innerText = ": الرابح هو";
-      document.querySelector(".span").innerText = winner;
-      winnerAudio.play();
-    } else {
-      if (eventElements.innerHTML == xmarkIcon) {
-        document.querySelector(".h2").innerText = " : الدور عند";
-        document.querySelector(".span").innerText = "o";
-      } else {
-        document.querySelector(".h2").innerText = " : الدور عند";
-        document.querySelector(".span").innerText = "x";
-      }
-    }
-  }
-}
-
-// restart the game function
-function playFun() {
-  all_i_ele.forEach((ele) => (ele.innerHTML = ""));
-  srick.className = "srick";
-  numberArray.fill(null);
-  all_i_ele.forEach(
-    (allElements) => (allElements.style.pointerEvents = "auto")
-  );
-}
+var pc = new App();
