dcf2e2d126c5ec9b2e5267b15edc93d25c1c0e58
Your Name <you@example.com>
2022-08-15 17:14:52 +0300

    update

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ class Main {
     // variables
     this.streak = document.querySelector(".streak");
     this.all_i_ele = document.querySelectorAll("span");
-    this.x_icon = '<i class="fa-solid fa-xmark"></i>';
-    this.o_icon = '<i class="fa-solid fa-o"></i>';
+    // this.x_icon = '<i class="fa-solid fa-xmark"></i>';
+    this.x_icon = '<img src="x.png" width="50" />';
+    this.o_icon = '<img src="o.png" width="50" />';
+    // this.o_icon = '<i class="fa-solid fa-o"></i>';
     this.printWord = document.querySelector(".h2");
     this.printPlayer = document.querySelector(".span");
     // for change between x and o
@@ -46,7 +48,10 @@ class Main {
 
     // restart the game
     this.play_button.addEventListener("click", this.playFun);
+
+   
   }
+
   //this function Choose to start playing with a person or a pc
   handlePersonFun = () => {
     // Store and check values and display the result
@@ -82,36 +87,52 @@ class Main {
     let index_Element = eventElements.dataset.index;
     // toggle between x and o
     if (this.changeIcons === this.x_icon) {
-      eventElements.innerHTML = this.x_icon;
-      this.numberArray[index_Element - 1] = "x";
-      this.changeIcons = this.o_icon;
-      this.audio.play();
+          eventElements.innerHTML = this.x_icon;
+          eventElements.style.pointerEvents = "none";
+          this.numberArray[index_Element - 1] = "x";
+          this.changeIcons = this.o_icon;
+          this.audio.play();
+     
     } else {
-      eventElements.innerHTML = this.o_icon;
-      this.numberArray[index_Element - 1] = "o";
-      this.changeIcons = this.x_icon;
-      this.audio.play();
+        eventElements.innerHTML = this.o_icon;
+        eventElements.style.pointerEvents = "none";
+        this.numberArray[index_Element - 1] = "o";
+        this.changeIcons = this.x_icon;
+        this.audio.play();
+       
     }
     // Check winner
     this.CheckWinner();
 
-    // Print which player will be playing
-    if (eventElements.innerHTML == this.x_icon) {
-      this.printWord.innerText = " : الدور عند";
-      this.printPlayer.innerText = "o";
-    } else {
-      this.printWord.innerText = " : الدور عند";
-      this.printPlayer.innerText = "x";
+    if(this.streak.classList.length > 1){
+      this.printWord.innerText = " the winner is  " ;
+      this.printPlayer.innerHTML = this.winner;
+    }else if(this.streak.classList.length == 1 &&
+       this.numberArray.includes(null)==false){
+        this.printWord.innerText = " Draw! " ;
+        this.printPlayer.innerHTML ="";
+    }else{
+    this.printWord.innerText = " turn at " ;
+    let g;
+    console.log(this.changeIcons.includes("x.png"))
+    this.changeIcons.includes("x.png")?true:g="o";
+    this.changeIcons.includes("o.png")?true:g="x";
+    this.printPlayer.innerHTML = g;
     }
+    
   };
   // restart the game function
   playFun = () => {
     this.all_i_ele.forEach((ele) => {
       ele.innerHTML = "";
       ele.style.pointerEvents = "auto";
+      ele.classList.remove("o");
+      ele.classList.remove("x");
     });
     this.streak.className = "streak";
     this.numberArray.fill(null);
+    this.printWord.innerText = "" ;
+    this.printPlayer.innerHTML = "";
   };
 }
 var person = new Main();
@@ -133,7 +154,8 @@ class App extends Main {
         let eventElements = event.target,
           index_Element = eventElements.dataset.index;
         if (this.changeIcons === this.x_icon) {
-          ele.innerHTML = this.x_icon;
+          eventElements.innerHTML = this.x_icon;
+          eventElements.style.pointerEvents = "none";
           this.numberArray[index_Element - 1] = "x";
           this.changeIcons = this.o_icon;
           this.audio.play();
@@ -141,7 +163,9 @@ class App extends Main {
             if (this.numberArray[this.number] === null) {
               this.numberArray[this.number] = "o";
               this.all_i_ele[this.number].innerHTML = this.o_icon;
+              this.all_i_ele[this.number].style.pointerEvents = "none";
               this.changeIcons = this.x_icon;
+              
               this.audio.play();
             } else {
               var nullValue = this.numberArray.indexOf(null);
@@ -149,6 +173,7 @@ class App extends Main {
                 this.numberArray[nullValue] = "o";
                 this.all_i_ele[nullValue].innerHTML = this.o_icon;
                 this.changeIcons = this.x_icon;
+                this.all_i_ele[nullValue].style.pointerEvents = "none";
                 this.audio.play();
               }
             }
@@ -156,16 +181,22 @@ class App extends Main {
         } else {
           this.numberArray[this.number] = "o";
           this.all_i_ele[this.number].innerHTML = this.o_icon;
+          this.all_i_ele[this.number].style.pointerEvents = "none";
           this.changeIcons = this.x_icon;
           this.audio.play();
         }
         // Check winner
         this.CheckWinner();
         if (this.streak.classList.length > 1) {
-          this.printWord.innerText = ": الرابح هو";
+          this.printWord.innerText ="the winner is ";
           this.printPlayer.innerText = this.winner;
-        } else {
-          this.printWord.innerText = " : الدور عند";
+        } else if(this.streak.classList.length == 1&&
+          this.numberArray.includes(null)==false){
+            this.printWord.innerText = "Draw!";
+          this.printPlayer.innerText = "";
+          }
+        else {
+          this.printWord.innerText = "turn at ";
           this.printPlayer.innerText = "x";
         }
       })
